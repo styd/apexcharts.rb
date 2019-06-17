ede116f5c6f0aa815c11d9aab87c25153bf17cf4
Adrian Setyadi <a.styd@yahoo.com>
2019-06-17 23:02:06 +0700

    Fix issue series and options mutation

--- lib/apexcharts/helper.rb
+++ lib/apexcharts/helper.rb
@@ -6,31 +6,32 @@ require_relative 'charts'
 module Apexcharts
   module Helper
     def line_chart series, options={}, &block
-      options[:id] ||= apexcharts_id
+      series, options = prepare_series_and_options(series, options)
       draw_chart(LineChart.new(series, options, &block))
     end
 
     def area_chart series, options={}, &block
-      options[:id] ||= apexcharts_id
+      series, options = prepare_series_and_options(series, options)
       draw_chart(AreaChart.new(series, options, &block))
     end
 
     def column_chart series, options={}, &block
-      options[:id] ||= apexcharts_id
+      series, options = prepare_series_and_options(series, options)
       draw_chart(ColumnChart.new(series, options, &block))
     end
 
     def bar_chart series, options={}, &block
-      options[:id] ||= apexcharts_id
+      series, options = prepare_series_and_options(series, options)
       draw_chart(BarChart.new(series, options, &block))
     end
 
     def scatter_chart series, options={}, &block
-      options[:id] ||= apexcharts_id
+      series, options = prepare_series_and_options(series, options)
       draw_chart(ScatterChart.new(series, options, &block))
     end
 
     def mixed_chart options={}, &block
+      options = deep_copy(options)
       options[:id] ||= apexcharts_id
       bindings = eval "self", block.binding
       draw_chart(MixedChart.new(options, bindings, &block))
@@ -38,6 +39,7 @@ module Apexcharts
     alias_method :combo_chart, :mixed_chart
 
     def syncing_chart options={}, &block
+      options = deep_copy(options)
       options[:group] ||= apexcharts_group
       bindings = eval "self", block.binding
       draw_chart(SyncingChart.new(options, bindings, &block))
@@ -45,12 +47,12 @@ module Apexcharts
     alias_method :synchronized_chart, :syncing_chart
 
     def pie_chart series, options={}
-      options[:id] ||= apexcharts_id
+      series, options = prepare_series_and_options(series, options)
       draw_chart(PieChart.new(series, options))
     end
 
     def donut_chart series, options={}
-      options[:id] ||= apexcharts_id
+      series, options = prepare_series_and_options(series, options)
       draw_chart(DonutChart.new(series, options))
     end
 
@@ -59,6 +61,13 @@ module Apexcharts
     def draw_chart chart
       chart = chart.render
       chart.respond_to?(:html_safe) ? chart.html_safe : chart
+    end
+
+    def prepare_series_and_options series, options
+      series = deep_copy(series)
+      options = deep_copy(options)
+      options[:id] ||= apexcharts_id
+      [series, options]
     end
 
     def apexcharts_id
@@ -71,6 +80,10 @@ module Apexcharts
       @_apexcharts_group ||= 0
       @_apexcharts_group += 1
       "chart-group-#{@_apexcharts_group}"
+    end
+
+    def deep_copy(options)
+      Marshal.load(Marshal.dump(options))
     end
   end
 end
