ccebf251f21f6161f0db3ed4f3eeb13ded961f56
Adrian Setyadi <a.styd@yahoo.com>
2019-06-18 21:38:43 +0700

    Fix red ci by setting TZ env to UTC

--- spec/utils/date_time_spec.rb
+++ spec/utils/date_time_spec.rb
@@ -7,7 +7,7 @@ RSpec.describe Apexcharts::Utils::DateTime do
     let(:converted) { described_class.convert(input) }
 
     context 'input is a Time' do
-      let(:input) { Time.parse("2019-06-18 20:20:20 +0700") }
+      let(:input) { Time.iso8601("2019-06-18T20:20:20+0700") }
 
       it 'converts correctly' do
         expect(converted).to eq 1560864020000
@@ -15,7 +15,7 @@ RSpec.describe Apexcharts::Utils::DateTime do
     end
 
     context 'input is a DateTime' do
-      let(:input) { DateTime.parse("2019-06-18T20:20:20+0700") }
+      let(:input) { DateTime.iso8601("2019-06-18T20:20:20+0700") }
 
       it 'converts correctly' do
         expect(converted).to eq 1560864020000
@@ -23,10 +23,18 @@ RSpec.describe Apexcharts::Utils::DateTime do
     end
 
     context 'input is a Date' do
-      let(:input) { Date.parse("2019-06-18") }
+      let(:input) { Date.iso8601("2019-06-18T00:00:00+07:00") }
+
+      before do
+        ENV["TZ"] = "UTC"
+      end
+
+      after do
+        ENV["TZ"] = nil
+      end
 
       it 'converts correctly' do
-        expect(converted).to eq 1560790800000
+        expect(converted).to eq 1560816000000
       end
     end
 
@@ -41,8 +49,16 @@ RSpec.describe Apexcharts::Utils::DateTime do
     context 'input is a string of Date' do
       let(:input) { "2019-06-18" }
 
+      before do
+        ENV["TZ"] = "UTC"
+      end
+
+      after do
+        ENV["TZ"] = nil
+      end
+
       it 'converts correctly' do
-        expect(converted).to eq 1560790800000
+        expect(converted).to eq 1560816000000
       end
     end
 
@@ -59,7 +75,7 @@ RSpec.describe Apexcharts::Utils::DateTime do
     let(:converted) { described_class.convert_range(input) }
 
     context 'input is a Time' do
-      let(:input) { Time.parse("2019-06-08 10:10:10 +0700")..Time.parse("2019-06-18 20:20:20 +0700") }
+      let(:input) { Time.iso8601("2019-06-08T10:10:10+0700")..Time.parse("2019-06-18 20:20:20 +0700") }
 
       it 'converts correctly' do
         expect(converted).to eq 1559963410000..1560864020000
@@ -67,7 +83,7 @@ RSpec.describe Apexcharts::Utils::DateTime do
     end
 
     context 'input is a DateTime' do
-      let(:input) { DateTime.parse("2019-06-08T10:10:10+0700")..DateTime.parse("2019-06-18T20:20:20+0700") }
+      let(:input) { DateTime.iso8601("2019-06-08T10:10:10+0700")..DateTime.parse("2019-06-18T20:20:20+0700") }
 
       it 'converts correctly' do
         expect(converted).to eq 1559963410000..1560864020000
@@ -75,10 +91,18 @@ RSpec.describe Apexcharts::Utils::DateTime do
     end
 
     context 'input is a Date' do
-      let(:input) { Date.parse("2019-06-08")..Date.parse("2019-06-18") }
+      let(:input) { Date.iso8601("2019-06-08")..Date.parse("2019-06-18") }
+
+      before do
+        ENV["TZ"] = "UTC"
+      end
+
+      after do
+        ENV["TZ"] = nil
+      end
 
       it 'converts correctly' do
-        expect(converted).to eq 1559926800000..1560790800000
+        expect(converted).to eq 1559952000000..1560816000000
       end
     end
 
@@ -93,8 +117,16 @@ RSpec.describe Apexcharts::Utils::DateTime do
     context 'input is a string of Date' do
       let(:input) { "2019-06-08".."2019-06-18" }
 
+      before do
+        ENV["TZ"] = "UTC"
+      end
+
+      after do
+        ENV["TZ"] = nil
+      end
+
       it 'converts correctly' do
-        expect(converted).to eq 1559926800000..1560790800000
+        expect(converted).to eq 1559952000000..1560816000000
       end
     end
 
@@ -111,7 +143,7 @@ RSpec.describe Apexcharts::Utils::DateTime do
     let(:type) { described_class.type(input) }
 
     context 'input is a Time' do
-      let(:input) { Time.parse("2019-06-18 20:20:20 +0700") }
+      let(:input) { Time.iso8601("2019-06-18T20:20:20+0700") }
 
       it 'converts correctly' do
         expect(type).to eq 'datetime'
@@ -119,7 +151,7 @@ RSpec.describe Apexcharts::Utils::DateTime do
     end
 
     context 'input is a DateTime' do
-      let(:input) { DateTime.parse("2019-06-18T20:20:20+0700") }
+      let(:input) { DateTime.iso8601("2019-06-18T20:20:20+0700") }
 
       it 'converts correctly' do
         expect(type).to eq 'datetime'
@@ -127,7 +159,7 @@ RSpec.describe Apexcharts::Utils::DateTime do
     end
 
     context 'input is a Date' do
-      let(:input) { Date.parse("2019-06-18") }
+      let(:input) { Date.iso8601("2019-06-18") }
 
       it 'converts correctly' do
         expect(type).to eq 'datetime'
