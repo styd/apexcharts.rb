eb1c56c4c404d75999e8c8de6a936d2f3947e182
Adrian Setyadi <a.styd@yahoo.com>
2019-07-14 22:52:50 +0700

    Freeze string literals to reduce garbage in memory

--- lib/apexcharts/charts/radial_bar.rb
+++ lib/apexcharts/charts/radial_bar.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 module Apexcharts
   class RadialBarChart < PolarChart
     def chart_type
--- lib/apexcharts/support/rails.rb
+++ lib/apexcharts/support/rails.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 module Apexcharts
   class Engine < ::Rails::Engine
     initializer "helper" do |app|
