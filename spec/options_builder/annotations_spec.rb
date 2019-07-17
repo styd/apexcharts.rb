8257bbb42ba2754626b888154890cdefcd4e0482
Adrian Setyadi <a.styd@yahoo.com>
2019-07-17 23:37:10 +0700

    Add the rest of OptionsBuilder tests and refactor

---
+++ spec/options_builder/annotations_spec.rb
@@ -0,0 +1,55 @@
+require 'spec_helper'
+
+RSpec.describe '#build_annotations' do
+  let(:x_sample) { nil }
+  let(:options) {
+    {
+      annotations: {
+        yaxis: [
+          {
+            y: 8200,
+            border_color: '#00E396',
+            label: {
+              border_color: '#00E396',
+              style: {
+                color: '#fff',
+                background: '#00E396',
+              },
+              text: 'Support'
+            }
+          }
+        ]
+      }
+    }
+  }
+  let(:ob) {
+    Apexcharts::OptionsBuilder.new(x_sample, options)
+  }
+  let(:expected_built) {
+    {
+      annotations: {
+        yaxis: [
+          {
+            y: 8200,
+            borderColor: '#00E396',
+            label: {
+              borderColor: '#00E396',
+              style: {
+                color: '#fff',
+                background: '#00E396',
+              },
+              text: 'Support'
+            }
+          }
+        ]
+      }
+    }
+  }
+
+  it 'only camelizes the hash keys' do
+    ob.build_annotations
+    expect(ob.built).to match(hash_including(expected_built))
+  end
+end
+
+
---
+++ spec/options_builder/stroke_spec.rb
@@ -0,0 +1,74 @@
+require 'spec_helper'
+
+RSpec.describe '#build_stroke' do
+  let(:x_sample) { nil }
+  let(:ob) {
+    Apexcharts::OptionsBuilder.new(x_sample, options)
+  }
+
+  context "string curve" do
+    let(:options) {
+      {
+        curve: 'straight'
+      }
+    }
+    let(:expected_built) {
+      {
+        stroke: {
+          curve: 'straight'
+        }
+      }
+    }
+
+    it 'adds show key with the boolean as the value' do
+      ob.build_stroke
+      expect(ob.built).to match(hash_including(expected_built))
+    end
+  end
+
+  context "boolean stroke" do
+    let(:options) {
+      {
+        stroke: false
+      }
+    }
+    let(:expected_built) {
+      {
+        stroke: {
+          show: false
+        }
+      }
+    }
+
+    it 'adds show key with the boolean as the value' do
+      ob.build_stroke
+      expect(ob.built).to match(hash_including(expected_built))
+    end
+  end
+
+  context "hash stroke" do
+    let(:options) {
+      {
+        stroke: {
+          show: true,
+          line_cap: 'round'
+        }
+      }
+    }
+    let(:expected_built) {
+      {
+        stroke: {
+          show: true,
+          lineCap: 'round',
+        }
+      }
+    }
+
+    it 'only camelizes the hash' do
+      ob.build_stroke
+      expect(ob.built).to match(hash_including(expected_built))
+    end
+  end
+end
+
+
