5ea655f2e5dba5e7b833974895fd24c8dd7ae37a
Adrian Setyadi <a.styd@yahoo.com>
2019-07-20 08:21:26 +0700

    Build colors

---
+++ spec/options_builder/colors_spec.rb
@@ -0,0 +1,64 @@
+require 'spec_helper'
+
+RSpec.describe '#build_colors' do
+  let(:x_sample) { nil }
+  let(:ob) {
+    ApexCharts::OptionsBuilder.new(x_sample, options)
+  }
+
+  context "colors is nil" do
+    let(:options) {
+      {
+        colors: nil
+      }
+    }
+    let(:expected_built) {
+      {
+        colors: nil
+      }
+    }
+
+    it 'return colors with nil value' do
+      ob.build_colors
+      expect(ob.built).to match(hash_including(expected_built))
+    end
+  end
+
+  context "colors is array" do
+    let(:options) {
+      {
+        colors: ['#FFAABB', '#AABB33']
+      }
+    }
+    let(:expected_built) {
+      {
+        colors: ['#FFAABB', '#AABB33']
+      }
+    }
+
+    it 'return colors with array value' do
+      ob.build_colors
+      expect(ob.built).to match(hash_including(expected_built))
+    end
+  end
+
+  context "colors is anything else" do
+    let(:options) {
+      {
+        colors: '#AABB33'
+      }
+    }
+    let(:expected_built) {
+      {
+        colors: ['#AABB33']
+      }
+    }
+
+    it 'return colors with array value' do
+      ob.build_colors
+      expect(ob.built).to match(hash_including(expected_built))
+    end
+  end
+end
+
+
