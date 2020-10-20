c8bc111345c27b9dacbd78a9063089989129d69d
Adrian Setyadi <a.styd@yahoo.com>
2020-10-21 05:45:37 +0700

    Fix failing fill spec for dry schema

--- lib/apex_charts/options/dry_schema/fill_options.rb
+++ lib/apex_charts/options/dry_schema/fill_options.rb
@@ -4,12 +4,25 @@ module ApexCharts::Options
   module DrySchema
     class FillOptions < Schema
       define do
-        optional(:colors)
-        optional(:gradient)
-        optional(:image)
-        optional(:opacity)
+        optional(:colors) { array? }
+        optional(:gradient).hash do
+          optional(:gradientToColors) { array? & each { str? } }
+          optional(:inverseColors) { bool? }
+          optional(:opacityFrom) { int? | float? }
+          optional(:opacityTo) { int? | float? }
+          optional(:shade) { str? }
+          optional(:shadeIntensity) { int? | float? }
+          optional(:stops) { array? & each { int? | float? } }
+          optional(:type) { str? }
+        end
+        optional(:image).hash do
+          optional(:src) { str? | (array? & each { str? }) }
+          optional(:width) { int? | float? }
+          optional(:height) { int? | float? }
+        end
+        optional(:opacity) { int? | float? | (array? & each { str? }) }
         optional(:pattern)
-        optional(:type)
+        optional(:type) { str? | (array? & each { str? }) }
       end
     end
   end
