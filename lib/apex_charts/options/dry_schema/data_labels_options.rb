a5c453a0b82eaf378ba68e451b7ed2a17842234b
Adrian Setyadi <a.styd@yahoo.com>
2020-11-14 08:51:27 +0700

    Fix failing data_labels spec for dry schema

--- lib/apex_charts/options/dry_schema/data_labels_options.rb
+++ lib/apex_charts/options/dry_schema/data_labels_options.rb
@@ -1,19 +1,36 @@
 # frozen_string_literal: true
 
+require_relative 'general_schema/function'
+require_relative 'general_schema/drop_shadow'
+
 module ApexCharts::Options
   module DrySchema
     class DataLabelsOptions < Schema
       define do
-        optional(:background)
-        optional(:distributed)
-        optional(:dropShadow)
-        optional(:enabled)
-        optional(:enabledOnSeries)
-        optional(:formatter)
-        optional(:offsetX)
-        optional(:offsetY)
-        optional(:style)
-        optional(:textAnchor)
+        optional(:background).hash do
+          optional(:borderColor) { str? }
+          optional(:borderRadius) { int? | float? }
+          optional(:borderWidth) { int? | float? }
+          optional(:dropShadow).hash(DropShadow)
+          optional(:enabled) { bool? }
+          optional(:foreColor) { str? }
+          optional(:opacity) { int? | float? }
+          optional(:padding) { int? | float? }
+        end
+        optional(:distributed) { bool? }
+        optional(:dropShadow).hash(DropShadow)
+        optional(:enabled) { bool? }
+        optional(:enabledOnSeries) { !nil? > (int? | float?) }
+        optional(:formatter).hash(Function)
+        optional(:offsetX) { int? | float? }
+        optional(:offsetY) { int? | float? }
+        optional(:style).hash do
+          optional(:colors) { array? & each { str? } }
+          optional(:fontFamily) { str? }
+          optional(:fontSize) { str? }
+          optional(:fontWeight) { str? | int? | float? }
+        end
+        optional(:textAnchor) { included_in? %w[start middle end] }
       end
     end
   end
